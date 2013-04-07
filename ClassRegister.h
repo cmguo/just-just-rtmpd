6ff4180f51815e5e0306c9eaf328485d7fdd63a9
isxxguo <isxxguo@pptv.com>
2013-04-07 02:15:10 +0000

    /ppbox/branches/1.2.0 /src/ppbox 调整 ClassFactory 中的 Class 注册实现，修正包含顺序错误引起的注册不生效问题 emailto: box-dev
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@6561 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ ClassRegister.h
@@ -0,0 +1,11 @@
+// ClassRegister.h
+
+#ifndef _PPBOX_RTMPD_CLASS_REGISTER_H_
+#define _PPBOX_RTMPD_CLASS_REGISTER_H_
+
+#include <ppbox/common/ClassRegister.h>
+
+#include "ppbox/rtmpd/RtmMuxer.h"
+#include "ppbox/rtmpd/RtmpPublishSink.h"
+
+#endif //_PPBOX_RTMPD_CLASS_REGISTER_H_
